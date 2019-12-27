20c1031a800141f4693252c7a4c372d0c0af930c
Nicholas Terrafranca <nterrafranca@gmail.com>
2019-12-27 10:30:20 -0500

    Basic user stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  before_save { email.downcase! }
+  before_save :downcase_email
 
   validates :first_name, :last_name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
@@ -8,5 +8,10 @@ class User < ApplicationRecord
 
   # enforces validations on the virtual `password` and `password_confirmation` attributes
   has_secure_password
+
+  private
+    def downcase_email
+      self.email.downcase!
+    end
 
 end
