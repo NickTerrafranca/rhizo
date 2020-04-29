9d3a9a1500ccb476313b4ed6e7de7d70c253de5f
Nicholas Terrafranca <nterrafranca@gmail.com>
2020-04-29 16:28:23 -0400

    Cleanup and QA

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ApplicationRecord
   has_many :recipe_line_items, dependent: :delete_all
   has_many :quantities, through: :recipe_line_items
   has_many :ingredients, through: :recipe_line_items
-  has_many :batches, dependent: :destroy
+  has_many :batches, dependent: :delete_all
 
   accepts_nested_attributes_for :recipe_line_items, reject_if: :all_blank, allow_destroy: true
 
