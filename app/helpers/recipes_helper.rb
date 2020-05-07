9ff8cd9fd51641fa5c6a4c02c8ce78a617a0ec05
Nicholas Terrafranca <nterrafranca@gmail.com>
2020-05-07 18:01:00 -0400

    Changing the display order of ingredient components

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,5 +1,5 @@
 module RecipesHelper
   def recipe_ingredient_line_item(recipe_line_item)
-    [recipe_line_item.quantity.amount, recipe_line_item.quantity.unit, recipe_line_item.ingredient.name].reject(&:blank?).join(' ')
+    [recipe_line_item.ingredient.name, ' - ', recipe_line_item.quantity.amount, recipe_line_item.quantity.unit].reject(&:blank?).join(' ')
   end
 end
