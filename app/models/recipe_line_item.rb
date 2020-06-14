f6b17288973e5ddd7ce1497dd0d9d5f03a3f7a86
Nicholas Terrafranca <nterrafranca@gmail.com>
2020-06-14 18:43:14 -0400

    When saving a recipe, check for existing ingredient and quantity and reference them rather than create duplicate records

--- app/models/recipe_line_item.rb
+++ app/models/recipe_line_item.rb
@@ -1,11 +1,11 @@
 class RecipeLineItem < ApplicationRecord
   has_one :recipe
-  belongs_to :quantity, dependent: :destroy
-  belongs_to :ingredient, dependent: :destroy
+  belongs_to :quantity
+  belongs_to :ingredient
 
-  before_save :find_or_create_ingredient
+  before_save :check_existing_ingredient, :check_existing_quantity
 
-  accepts_nested_attributes_for :quantity, :ingredient#, allow_destroy: true
+  accepts_nested_attributes_for :quantity, :ingredient
 
   def calculate_batch_amount(batch)
     converted_quantity = quantity.amount * batch.multiplier
@@ -16,16 +16,22 @@ class RecipeLineItem < ApplicationRecord
     end
   end
 
-
-
   private
 
-  def find_or_create_ingredient
-    # binding.pry
-    # if Ingredient.find(self.ingredient_id)
-    #   ing_name  = Ingredient.find(self.ingredient_id).name
-    #   Ingredient.where(name: ing_name).order(:desc)
-    # end
-  end
+    def check_existing_ingredient
+      new_record = Ingredient.find self.ingredient_id
+      if first_existing = Ingredient.where(name: new_record.name).where.not(id: new_record.id).order(created_at: :asc).first
+        self.ingredient_id = first_existing.id
+        new_record.destroy
+      end
+    end
+
+    def check_existing_quantity
+      new_record = Quantity.find(self.quantity_id)
+      if first_existing = Quantity.where(unit: new_record.unit, amount: new_record.amount).where.not(id: new_record.id).order(created_at: :asc).first
+        self.quantity_id = first_existing.id
+        new_record.destroy
+      end
+    end
 
 end
