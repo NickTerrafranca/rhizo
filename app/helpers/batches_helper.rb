269c37630606e1adc2f396369410e076a5ba6620
Nicholas Terrafranca <nterrafranca@gmail.com>
2020-06-14 18:47:43 -0400

    A bit of display logic and cleanup

--- app/helpers/batches_helper.rb
+++ app/helpers/batches_helper.rb
@@ -1,5 +1,5 @@
 module BatchesHelper
   def converted_recipe_line_item(line_item: recipe_line_item, batch: recipe_batch)
-    [line_item.ingredient.name, ' - ', line_item.calculate_batch_amount(@batch), line_item.quantity.unit].reject(&:blank?).join(' ')
+    [line_item.ingredient.name, ' - ', line_item.calculate_batch_amount(batch), line_item.quantity.unit].reject(&:blank?).join(' ')
   end
 end
