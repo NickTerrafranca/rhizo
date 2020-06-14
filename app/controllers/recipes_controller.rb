8e0f2eef6e4d0cf31f343334e695162fbcc8f7cb
Nicholas Terrafranca <nterrafranca@gmail.com>
2020-06-14 18:46:50 -0400

    Add functionality do delete recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -39,6 +39,14 @@ class RecipesController < ApplicationController
   end
 
   def destroy
+    @recipe = Recipe.find(params[:id])
+    if @recipe.destroy
+      flash[:success] = 'Recipe deleted'
+      redirect_to recipes_path
+    else
+      flash.now[:error] = 'Unable to delete'
+      render 'edit'
+    end
   end
 
   private
